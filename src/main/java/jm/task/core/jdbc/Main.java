1071b78753bca10eca968e17712068228e4a99af
Aleksey <13Alexnik@gmail.com>
2024-05-01 17:04:56 +0300

    change_1

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,30 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
-        // реализуйте алгоритм здесь
-    }
+
+
+    public static void main(String[] args) throws SQLException {
+
+            Util.getConnection();
+            UserDao userDao = new UserDaoJDBCImpl();
+
+            userDao.createUsersTable();
+
+            userDao.saveUser("Anna", "PostAnna", (byte) 11);
+            userDao.saveUser("Boris", "PostBoris", (byte) 22);
+            userDao.saveUser("Clara", "PostClara", (byte) 33);
+            userDao.saveUser("Demon", "PostDemon", (byte) 44);
+
+            userDao.removeUserById(1);
+            userDao.getAllUsers();
+            userDao.cleanUsersTable();
+            userDao.dropUsersTable();
+        }
+
 }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,49 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.*;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
+
     public UserDaoJDBCImpl() {
 
     }
 
-    public void createUsersTable() {
+    public void createUsersTable() throws SQLException {
+        try (Connection connection = Util.getConnection();
+             Statement statement = connection.createStatement()) {
+            DatabaseMetaData metaData = connection.getMetaData();
+            ResultSet tables = metaData.getTables(null, null, "newtable", null);
+            if (!tables.next()) {
+                String s = "CREATE TABLE `newtable`(id BIGINT NOT NULL AUTO_INCREMENT, name VARCHAR(45) NOT NULL, " +
+                        "lastName VARCHAR(45) NOT NULL , age TINYINT NOT NULL , PRIMARY KEY (id))";
+                statement.executeUpdate(s);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void dropUsersTable() throws SQLException {
 
     }
 
-    public void dropUsersTable() {
+    public void saveUser(String name, String lastName, byte age) throws SQLException {
 
     }
 
-    public void saveUser(String name, String lastName, byte age) {
+    public void removeUserById(long id) throws SQLException {
 
     }
 
-    public void removeUserById(long id) {
-
-    }
-
-    public List<User> getAllUsers() {
+    public List<User> getAllUsers() throws SQLException {
         return null;
     }
 
-    public void cleanUsersTable() {
+    public void cleanUsersTable() throws SQLException {
 
     }
 }
